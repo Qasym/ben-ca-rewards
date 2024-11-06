e2065e7585550c95b837a983833e2b76f432533a
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-07 07:14:12 +0900

    init commit

---
+++ src/test/kotlin/com/kasymzhan/rewards/RewardsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kasymzhan.rewards
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RewardsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
