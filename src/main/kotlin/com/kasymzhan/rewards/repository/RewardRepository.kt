411aea317d64443cef5d18f6d981aab8836fd2a4
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-10 16:13:31 +0900

    moving to objectId as id

--- src/main/kotlin/com/kasymzhan/rewards/repository/RewardRepository.kt
+++ src/main/kotlin/com/kasymzhan/rewards/repository/RewardRepository.kt
@@ -1,6 +1,7 @@
 package com.kasymzhan.rewards.repository
 
 import com.kasymzhan.rewards.data.Reward
+import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface RewardRepository : MongoRepository<Reward, Long> {}
+interface RewardRepository : MongoRepository<Reward, ObjectId> {}
--- src/main/kotlin/com/kasymzhan/rewards/utils/DbConstants.kt
+++
@@ -1,5 +0,0 @@
-package com.kasymzhan.rewards.utils
-
-object DbConstants {
-    const val COUNTER_ID = "rewards_counter"
-}
