bd2accb142ce17dbca272d2d62cc793e12025845
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-10 17:31:22 +0900

    fixed critical issue with collation vs collection; added enums for best practices

--- src/main/kotlin/com/kasymzhan/rewards/data/Reward.kt
+++ src/main/kotlin/com/kasymzhan/rewards/data/Reward.kt
@@ -1,13 +1,21 @@
 package com.kasymzhan.rewards.data
 
+import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document(collation = "rewards")
+@Document(collection = "rewards")
 data class Reward(
     @Id
-    val id: Long,
+    val id: ObjectId,
     var name: String,
-    var item: String,
-    var quantity: String,
+    var item: RewardItem,
+    var quantity: Int,
 )
+
+enum class RewardItem(private val type: String) {
+    GOLD("GOLD"),
+    DIAMOND("DIAMOND");
+
+    fun getName(): String = type
+}
