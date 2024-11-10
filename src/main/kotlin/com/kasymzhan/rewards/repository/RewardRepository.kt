2e9edb574a6dabc21c52daffa3159359eeadbe61
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-10 17:31:56 +0900

    added missing @Repository tag

--- src/main/kotlin/com/kasymzhan/rewards/repository/RewardRepository.kt
+++ src/main/kotlin/com/kasymzhan/rewards/repository/RewardRepository.kt
@@ -3,5 +3,7 @@ package com.kasymzhan.rewards.repository
 import com.kasymzhan.rewards.data.Reward
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface RewardRepository : MongoRepository<Reward, ObjectId> {}
