f8e8a2073e9300ef042107d3c2a7ac9be209eadc
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-10 17:32:36 +0900

    added data types for req/res

---
+++ src/main/kotlin/com/kasymzhan/rewards/data/RewardRequest.kt
@@ -0,0 +1,8 @@
+package com.kasymzhan.rewards.data
+
+data class RewardRequest(
+    val name: String,
+    val item: String,
+    val quantity: Int
+)
+
---
+++ src/main/kotlin/com/kasymzhan/rewards/data/RewardResponse.kt
@@ -0,0 +1,7 @@
+package com.kasymzhan.rewards.data
+
+data class RewardResponse(
+    val name: String,
+    val item: String,
+    val quantity: Int
+)
