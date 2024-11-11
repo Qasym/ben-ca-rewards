8b60a48b6b972979a76de59d8fb40aa07121c4c7
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-11 18:00:29 +0900

    changed id type to String to avoid issues during serialization/deserialization

--- src/main/kotlin/com/kasymzhan/rewards/data/RewardResponse.kt
+++ src/main/kotlin/com/kasymzhan/rewards/data/RewardResponse.kt
@@ -1,9 +1,7 @@
 package com.kasymzhan.rewards.data
 
-import org.bson.types.ObjectId
-
 data class RewardResponse(
-    val id: ObjectId,
+    val id: String,
     val name: String,
     val item: String,
     val quantity: Int
